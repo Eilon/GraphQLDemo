fd07d79318e3e6369f81e99ed707421de7a5a998
Eilon Lipton <Eilon@users.noreply.github.com>
2019-08-01 14:36:44 -0700

    Add basic GraphQL functionality for movie list

---
+++ AspNetCoreGraphQL.Server/GraphQLModels/MoviesGraphQLSchema.cs
@@ -0,0 +1,59 @@
+﻿using AspNetCoreGraphQL.Server.DataAccess;
+using AspNetCoreGraphQL.Server.DataModels;
+using GraphQL;
+using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreGraphQL.Server.GraphQLModels
+{
+    public class MoviesGraphQLSchema : Schema
+    {
+        public MoviesGraphQLSchema(IDependencyResolver resolver) 
+            : base(resolver)
+        {
+            Query = resolver.Resolve<MoviesDbQuery>();
+            //Mutation = new MoviesDbMutation(...);
+            //Subscription = new MoviesDbSubscriptions(...);
+        }
+    }
+
+    public class MoviesDbQuery : ObjectGraphType
+    {
+        public MoviesDbQuery(MoviesDbContext moviesDbContext)
+        {
+            Name = "MoviesDb";
+            Description = "A database of movies, actors, and characters.";
+
+            Field<ListGraphType<MovieType>>("movies", resolve: context => moviesDbContext.Movies, description: "All movies.");
+
+            FieldAsync<MovieType>(
+                "movie",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "id" }),
+                resolve: async context =>
+                {
+                    var id = context.GetArgument<int>("id");
+                    return await moviesDbContext.Movies.FindAsync(id);
+                }
+            );
+
+        }
+    }
+
+    public class MovieType : ObjectGraphType<Movie>
+    {
+        public MovieType()
+        {
+            Field(x => x.ID, type: typeof(IdGraphType)).Name("id");
+
+            Field(x => x.Title).Description("Movie title.");
+            Field(x => x.Year).Description("Movie release year.");
+            Field(x => x.Language).Description("Movie language.");
+
+            //Field(x => x.Genre, type: typeof(GenreType)).Description("Movie genre.");
+
+        }
+    }
+}
--- AspNetCoreGraphQL.Server/Startup.cs
+++ AspNetCoreGraphQL.Server/Startup.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AspNetCoreGraphQL.Server.DataAccess;
+using AspNetCoreGraphQL.Server.GraphQLModels;
+using GraphQL;
+using GraphQL.Server;
+using GraphQL.Server.Ui.GraphiQL;
+using GraphQL.Server.Ui.Playground;
+using GraphQL.Server.Ui.Voyager;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,12 +21,14 @@ namespace AspNetCoreGraphQL.Server
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnvironment)
         {
             Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment HostingEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -29,9 +37,30 @@ namespace AspNetCoreGraphQL.Server
             {
                 options.UseSqlServer(Configuration.GetConnectionString("MoviesDb"));
             });
-
-
+            
             services.AddRazorPages();
+
+            // Temporary workaround. See https://github.com/graphql-dotnet/graphql-dotnet/issues/1116
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+
+
+            // GraphQL
+            services.AddScoped<IDependencyResolver>(s => new FuncDependencyResolver(s.GetRequiredService));
+            services.AddScoped<MoviesGraphQLSchema>();
+
+            services.AddGraphQL(options =>
+                {
+                    options.EnableMetrics = true;
+                    options.ExposeExceptions = HostingEnvironment.IsDevelopment();
+                })
+                .AddGraphTypes(ServiceLifetime.Scoped)
+                .AddRelayGraphTypes();
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +78,20 @@ namespace AspNetCoreGraphQL.Server
             }
 
             app.UseHttpsRedirection();
+
+            // use HTTP middleware at path /graphql
+            app.UseGraphQL<MoviesGraphQLSchema>("/graphql");
+
+            // use graphiQL middleware at default url /graphiql
+            app.UseGraphiQLServer(new GraphiQLOptions());
+
+            // use graphql-playground middleware at default url /ui/playground
+            app.UseGraphQLPlayground(new GraphQLPlaygroundOptions());
+
+            // use voyager middleware at default url /ui/voyager
+            app.UseGraphQLVoyager(new GraphQLVoyagerOptions());
+
+
             app.UseStaticFiles();
 
             app.UseRouting();
